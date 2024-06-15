1a2f20867d3ec5ea39c3cab4cb0f6e26e50afa75
tosaken1116 <nagato20021122@gmail.com>
2024-06-15 21:08:31 +0900

    feat: Add useAudio hook for playing and stopping audio files

---
+++ src/hooks/audio/index.ts
@@ -0,0 +1,24 @@
+import { useEffect, useRef } from "react";
+
+export const useAudio = (fileName: string) => {
+	const audioRef = useRef<HTMLAudioElement | null>(null);
+
+	useEffect(() => {
+		audioRef.current = new Audio(`audio/${fileName}`);
+	}, [fileName]);
+
+	const play = () => {
+		if (audioRef.current) {
+			audioRef.current.play();
+		}
+	};
+
+	const stop = () => {
+		if (audioRef.current) {
+			audioRef.current.pause();
+			audioRef.current.currentTime = 0;
+		}
+	};
+
+	return { play, stop };
+};
