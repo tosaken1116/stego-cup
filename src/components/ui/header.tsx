d65f4d9cd3d9aee53fc7b2b5badb2615a6b15428
tosaken1116 <nagato20021122@gmail.com>
2024-06-15 21:05:12 +0900

    feat: Update Header component for improved layout and branding

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,17 +1,18 @@
+import { SERVICE_NAME } from "@/constants";
 import { cn } from "@/lib/utils";
 import { Mochiy_Pop_One } from "next/font/google";
 
 const mochiyPopOne = Mochiy_Pop_One({
-  subsets: ["latin"],
-  weight: ["400"],
-  style: ["normal"],
+	subsets: ["latin"],
+	weight: ["400"],
+	style: ["normal"],
 });
 export const Header = () => {
-  return (
-    <header className="fixed left-0 top-0 p-4">
-      <h1 className={cn("text-4xl font-bold", mochiyPopOne.className)}>
-        タイピング99
-      </h1>
-    </header>
-  );
+	return (
+		<header className="fixed top-0 left-0 p-4">
+			<h1 className={cn("font-bold text-4xl", mochiyPopOne.className)}>
+				{SERVICE_NAME}
+			</h1>
+		</header>
+	);
 };
