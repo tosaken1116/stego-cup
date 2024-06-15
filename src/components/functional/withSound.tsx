708369c5e470cdb8bb50756e3210c3ffa03371f4
tosaken1116 <nagato20021122@gmail.com>
2024-06-15 21:10:58 +0900

    feat: Refactor withClickSound and withHoverSound higher-order components

--- src/components/functional/withSound.tsx
+++ src/components/functional/withSound.tsx
@@ -3,60 +3,60 @@ import Head from "next/head";
 import { type ComponentType, useEffect, useRef } from "react";
 
 export const withClickSound = (
-	Component: React.ComponentType,
-	fileName: string,
+  Component: React.ComponentType,
+  fileName: string
 ) => {
-	return () => {
-		const audioRef = useRef<HTMLAudioElement | null>(null);
+  return () => {
+    const audioRef = useRef<HTMLAudioElement | null>(null);
 
-		useEffect(() => {
-			audioRef.current = new Audio(`audio/${fileName}`);
-		}, [fileName]);
+    useEffect(() => {
+      audioRef.current = new Audio(`audio/${fileName}`);
+    }, [fileName]);
 
-		const play = () => {
-			if (audioRef.current) {
-				audioRef.current.play();
-			}
-		};
+    const play = () => {
+      if (audioRef.current) {
+        audioRef.current.play();
+      }
+    };
 
-		return (
-			<div onClick={play}>
-				<Component />
-			</div>
-		);
-	};
+    return (
+      <div onClick={play}>
+        <Component />
+      </div>
+    );
+  };
 };
 
 export const withHoverSound = (Component: ComponentType, fileName: string) => {
-	return () => {
-		const audioRef = useRef<HTMLAudioElement | null>(null);
+  return () => {
+    const audioRef = useRef<HTMLAudioElement | null>(null);
 
-		const play = () => {
-			if (audioRef.current) {
-				audioRef.current.play();
-			}
-		};
+    const play = () => {
+      if (audioRef.current) {
+        audioRef.current.play();
+      }
+    };
 
-		useEffect(() => {
-			audioRef.current = new Audio(`audio/${fileName}`);
-		}, [fileName]);
-		return (
-			<>
-				<Head>
-					<link rel="preload" href={`audio/${fileName}`} as="audio" />
-				</Head>
-				<div
-					onMouseEnter={play}
-					onMouseLeave={() => {
-						if (audioRef.current) {
-							audioRef.current.pause();
-							audioRef.current.currentTime = 0;
-						}
-					}}
-				>
-					<Component />
-				</div>
-			</>
-		);
-	};
+    useEffect(() => {
+      audioRef.current = new Audio(`audio/${fileName}`);
+    }, [fileName]);
+    return (
+      <>
+        <Head>
+          <link rel="preload" href={`audio/${fileName}`} as="audio" />
+        </Head>
+        <div
+          onMouseEnter={play}
+          onMouseLeave={() => {
+            if (audioRef.current) {
+              audioRef.current.pause();
+              audioRef.current.currentTime = 0;
+            }
+          }}
+        >
+          <Component />
+        </div>
+      </>
+    );
+  };
 };
