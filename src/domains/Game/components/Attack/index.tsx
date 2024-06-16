cc9c8210904ec7868fc1348a64ebca0fa5f5aab0
tosaken1116 <nagato20021122@gmail.com>
2024-06-16 13:27:36 +0900

    chore: Refactor Attack component to improve code readability and formatting

--- src/domains/Game/components/Attack/index.tsx
+++ src/domains/Game/components/Attack/index.tsx
@@ -3,8 +3,8 @@ import { useConnection } from "../../hooks/useConnect";
 import type { Attack as AttackType, UserState } from "../../types";
 import { AttackEffect } from "../AttackEffect";
 type Position = {
-	x: number;
-	y: number;
+  x: number;
+  y: number;
 };
 
 const cellWidth = 125;
@@ -12,51 +12,52 @@ const cellHeight = 45;
 const gap = 4;
 const columns = 11;
 const getUserPosition = (
-	userID: string,
-	userIDs: string[],
+  userID: string,
+  userIDs: string[]
 ): Position | null => {
-	const index = userIDs.indexOf(userID);
-	if (index === -1) {
-		return null;
-	}
+  const index = userIDs.indexOf(userID);
+  if (index === -1) {
+    return null;
+  }
 
-	const row = Math.floor(index / columns);
-	const col = index % columns;
+  const row = Math.floor(index / columns);
+  const col = index % columns;
 
-	const x = col * (cellWidth + gap);
-	const y = row * (cellHeight + gap);
+  const x = col * (cellWidth + gap);
+  const y = row * (cellHeight + gap);
 
-	return { x, y };
+  return { x, y };
 };
 export const Attack = ({
-	attack,
-	userIDs,
+  attack,
+  userIDs,
 }: {
-	attack: AttackType[];
-	userIDs: UserState["id"][];
+  attack: AttackType[];
+  userIDs: UserState["id"][];
 }) => {
-	const { user } = useAuthUseCase();
-	const { popAttack } = useConnection();
-	const items = attack
-		.map((a) => {
-			const attacker =
-				user?.uid === a.from
-					? { x: 700, y: 200 }
-					: getUserPosition(a.from, userIDs);
-			const target =
-				user?.uid === a.to
-					? { x: 700, y: 200 }
-					: getUserPosition(a.to, userIDs);
-			if (attacker === null || target === null) {
-				return null;
-			}
-			return {
-				x: attacker.x - 300,
-				y: attacker.y - 100,
-				endX: target.x - 200,
-				endY: target.y - 100,
-			};
-		})
-		.filter((a): a is NonNullable<typeof a> => a !== null);
-	return <AttackEffect items={items} deleteAnimate={popAttack} />;
+  const { user } = useAuthUseCase();
+  const { popAttack } = useConnection();
+  const items = attack
+    .map((a) => {
+      const attacker =
+        user?.uid === a.from
+          ? { x: 700, y: 200 }
+          : getUserPosition(a.from, userIDs);
+      const target =
+        user?.uid === a.to
+          ? { x: 700, y: 200 }
+          : getUserPosition(a.to, userIDs);
+      if (attacker === null || target === null) {
+        return null;
+      }
+      return {
+        x: attacker.x - 300,
+        y: attacker.y - 100,
+        endX: target.x - 200,
+        endY: target.y - 100,
+        damage: a.damage,
+      };
+    })
+    .filter((a): a is NonNullable<typeof a> => a !== null);
+  return <AttackEffect items={items} deleteAnimate={popAttack} />;
 };
